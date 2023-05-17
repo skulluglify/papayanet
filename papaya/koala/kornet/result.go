0d15542111df5bd63da4a3a0293451be47cf3427
skulluglify <skulluglify@outlook.com>
2023-05-17 10:44:11 +0700

    feat/randomize id by timestamp

--- papaya/koala/kornet/result.go
+++ papaya/koala/kornet/result.go
@@ -2,6 +2,7 @@ package kornet
 
 import (
   "math/rand"
+  "time"
 )
 
 type Result struct {
@@ -16,9 +17,10 @@ type Result struct {
 func ResultNew(message *Message, data any) *Result {
 
   // fake result ID, just wrapping message and data
+  randomize := rand.New(rand.NewSource(time.Now().UnixNano()))
 
   return &Result{
-    ID:      uint(rand.Uint32()), // randomized
+    ID:      uint(randomize.Uint32()), // randomized
     Logs:    make([]string, 0),
     Status:  "Unknown", // Modify by Context
     Message: message.Message,
